d55fd84eca5e764ae572318863aaa5f51210b7ce
sewastian <sebastianrincon834@gmail.com>
2023-08-31 18:14:32 -0500

    feat [models/supplier -> supplier.js]

---
+++ models/supplier/supplies.js
@@ -0,0 +1,19 @@
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema
+
+// const schemaSupplies = new Schema({
+
+//     supplier:{
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Supplier'
+//     },
+//     product:{
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Products'
+//     },
+//     purchasePrice:{
+//         type: Number
+//     }
+// })
+
+// module.exports = mongoose.model('Supplies',schemaSupplies)
