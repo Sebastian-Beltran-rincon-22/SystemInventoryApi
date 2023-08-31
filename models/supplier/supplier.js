c8627da5a8114792f993e499989a114546d7392a
sewastian <sebastianrincon834@gmail.com>
2023-08-31 18:13:42 -0500

    feat [models/supplier -> supplier.js]

---
+++ models/supplier/supplier.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const supplierSchema = new Schema({
+
+    name:{
+        type: String
+    },
+    address:{
+        type: String
+    },
+    phone:{
+        type: Number,
+        required: true,
+        maxLenth: 15,
+        minLenth: 5
+    },
+    email:{
+        type: String,
+        unique: true,
+        validate: {
+            validator: function(emailUs) {
+                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(emailUs);
+            },
+            message: props => `${props.value} is not a valid email`
+        },
+        required: [true, 'user email required']
+    }
+})
+
+module.exports = mongoose.model('Supplier',supplierSchema)
