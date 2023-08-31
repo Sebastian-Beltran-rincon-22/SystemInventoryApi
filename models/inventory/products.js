9306505dc6b40c086dac091020ae3a5730a5db3d
sewastian <sebastianrincon834@gmail.com>
2023-08-31 18:13:13 -0500

    feat [models/inventory -> products.js]

---
+++ models/inventory/products.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const productsSchema = new Schema ({
+
+    nameProducts:{
+        type: String,
+        required: true
+    },
+    imgProduct:{
+        type: String
+    },
+    brand:{
+        type: String
+    },
+    description:{
+        type: String
+    },
+    price:{
+        type: Number
+    },
+    entryDate:{
+        type: Date,
+        default: Date.now
+    },
+    availability:{
+        type: Boolean // Hacer Pruebas para ver si es necesario en Product 
+    },
+    availableQuantity:{
+        type: Number
+    },
+
+    category:{ // Relacion de uno a mucho => asignar Categoria para traer los productos con esa categoria
+        type: mongoose.Schema.Types.String,
+        ref: 'Category'
+    }
+
+},{versionKey:false})
+
+module.exports = mongoose.model('Products',productsSchema)
