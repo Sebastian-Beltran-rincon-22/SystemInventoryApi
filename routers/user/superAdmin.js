eafac8abb3a95777eb8f348e3d18abef918a0b03
sewastian <sebastianrincon834@gmail.com>
2023-09-02 01:19:21 -0500

    update [routers/user -> superAdmin.js]

--- routers/user/superAdmin.js
+++ routers/user/superAdmin.js
@@ -1,12 +1,23 @@
 const express = require ('express')
 const router = express.Router()
-const superAdmincontroller = require ('../../controllers/user/superAdmin')
+const superAdminCon = require('../../controllers/user/superAdmin')
 
-router.get('/', superAdmincontroller.getAdmins)
-router.get('/:id', superAdmincontroller.getById)
-router.post('/', superAdmincontroller.createById)
-router.patch('/:id', superAdmincontroller.updateById)
-router.delete('/:id', superAdmincontroller.deleteById)
+router.use((req, res, next) => {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+    });
+
+router.post('/signup',superAdminCon.signup)
+router.delete('/:id', superAdminCon.deleteUser)
+
+// router.get('/', superAdmincontroller.getAdmins)
+// router.get('/:id', superAdmincontroller.getById)
+// router.post('/', superAdmincontroller.createById)
+// router.patch('/:id', superAdmincontroller.updateById)
+// router.delete('/:id', superAdmincontroller.deleteById)
 
 
-
+module.exports = router
