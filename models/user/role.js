724a746942e31e8d64868befa581b35ee3684957
sewastian <sebastianrincon834@gmail.com>
2023-09-04 19:56:55 -0500

    feat [models/user -> role.js]

---
+++ models/user/role.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ROLE = ['admin', 'user']
+
+const roleSchema = new Schema({
+
+    name:String
+
+},{versionKey:false})
+
+const Admin = mongoose.model('Admin',roleSchema)
+
+module.exports = {
+
+    Admin:Admin,
+
+    ROLE: ROLE
+}
