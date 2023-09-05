9265e8577355abdc9b0c2677865d1c0727ef8713
sewastian <sebastianrincon834@gmail.com>
2023-09-04 19:53:34 -0500

    update [index.js]

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const app = require('./app')
 const dataBase = require('./database')
 const mongoose = require('mongoose')
+const creatingRole = require ('./libs/initialSetup')
 require(`dotenv`).config()
 
 
 const port = 3000 // conectec in port
 app.listen(port)
 console.log('server listen on port', port)
+console.log(creatingRole.createRoles)
 
