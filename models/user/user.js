dd0b2fb3ac8f1af127b8412039e929367b989c0b
sewastian <sebastianrincon834@gmail.com>
2023-09-04 19:57:24 -0500

    feat [models/user -> user.js]

---
+++ models/user/user.js
@@ -0,0 +1,61 @@
+const mongoose = require ('mongoose')
+const bcrypt = require('bcrypt')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema ({
+
+    nickName:{
+        type: String,
+        required: true,
+        unique: true,
+        maxLenth: 100
+    },
+    email:{
+        type: String,
+        unique: true,
+        validate: {
+            validator: function(email) {
+                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email);
+            },
+            message: props => `${props.value} is not a valid email`
+        },
+        required: [true, 'user email required']
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    lastConnect:{
+        type: Date,
+        default: null
+    },
+
+    roles:[{
+        ref: "Admin",
+        type: mongoose.Schema.Types.ObjectId
+    }],
+
+    
+},{versionKey:false}) 
+
+userSchema.statics.encryptPassword  = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+
+userSchema.statics.comparePassword = async (password, receivedPassword) => {
+    return await bcrypt.compare(password, receivedPassword)
+}
+
+userSchema.pre("save", async function (next) {
+    const admin = this;
+    if (!admin.isModified("password")) {
+        return next();
+    }
+    const hash = await bcrypt.hash(admin.password, 10);
+    admin.password = hash;
+    next();
+})
+
+module.exports = mongoose.model('User',userSchema)
+
